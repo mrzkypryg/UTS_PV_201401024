a7c9125b6244b5ea1d17246fdddd2d508de2ea62
Muhammad Rizky Prayoga_024 <mrpharyga2019@gmail.com>
2022-03-31 09:29:51 +0700

    Jawaban UTS 201401024 Muhammad Rizky Prayoga

---
+++ Soal No 2 (Program Pert 5 dan 6)/Pert 6 Kasir SQL Database/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KasirBaru_Pertemuan6
+{
+    public partial class Form1 : Form
+    {
+        private SqlCommand cmd;
+        private DataSet ds;
+        private SqlDataAdapter da;
+        readonly Koneksi1 Konn = new Koneksi1();
+        public Form1()
+        {
+            InitializeComponent();
+            SqlConnection conn = Konn.GetConn();
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand("Select * from TBL_BARANG", conn);
+                ds = new DataSet();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "TBL_BARANG");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "TBL_BARANG";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception G)
+            {
+                MessageBox.Show(G.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
---
+++ Soal No 2 (Program Pert 5 dan 6)/Pert 6 Kasir SQL Database/Koneksi1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace KasirBaru_Pertemuan6
+{
+    internal class Koneksi1
+    {
+        public SqlConnection GetConn()
+        {
+            SqlConnection Conn = new SqlConnection
+            {
+                ConnectionString = "Data Source=DESKTOP-I8ILPVI\\SQLEXPRESS;Initial Catalog=Kasir;Integrated Security=True"
+            };
+            return Conn;
+
+        }
+    }
+}
